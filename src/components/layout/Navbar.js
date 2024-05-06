7e1757f4fb0069a9cab9465a55bd576639d95860
ali-fadaei23 <razorn1375@gmail.com>
2024-05-06 18:04:09 +0330

    Add navbar

---
+++ src/components/layout/Navbar.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+
+export default class Navbar extends Component {
+  render() {
+    return (
+      <nav className='navbar bg-primary'>
+        <i class='fa-brands fa-github'></i>
+        <h1>Navbar</h1>
+      </nav>
+    );
+  }
+}
