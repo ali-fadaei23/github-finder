db117f9df74100a35730c7d76637514368255ac2
ali-fadaei23 <razorn1375@gmail.com>
2024-05-06 20:19:20 +0330

    Add Brand Icon

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,11 +1,24 @@
 import React, { Component } from "react";
+import PropTypes from "prop-types";
 
 export default class Navbar extends Component {
+  static defaultProps = {
+    title: "Github Finder",
+    icon: "fa-brands fa-github",
+  };
+
+  static propTypes = {
+    title: PropTypes.string.isRequired,
+    icon: PropTypes.string.isRequired,
+  };
+
   render() {
     return (
       <nav className='navbar bg-primary'>
-        <i class='fa-brands fa-github'></i>
-        <h1>Navbar</h1>
+        <h1>
+          <i class={this.props.icon}></i>
+          {this.props.title}
+        </h1>
       </nav>
     );
   }
