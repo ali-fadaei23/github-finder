17d9a8e8b569755fc5cc01b1fa51d11163ce0d81
ali-fadaei23 <razorn1375@gmail.com>
2024-05-07 10:41:45 +0330

    Update users

--- src/components/users/UserItem.jsx
+++ src/components/users/UserItem.jsx
@@ -1,29 +1,22 @@
 import React, { Component } from "react";
 
 export class UserItem extends Component {
-  constructor() {
-    super();
-    this.state = {
-      id: "id",
-      login: "mojombo",
-      avatar_url: "https://avatars.githubusercontent.com/u/1?v=4",
-      html_url: "https://github.com/mojombo",
-    };
-  }
-
   render() {
+    const { login, avatar_url, html_url } = this.props.user;
     return (
       <div className='card text-center'>
         <img
-          src={this.state.avatar_url}
+          src={avatar_url}
           alt=''
           className='round-img'
           style={{ width: "60px" }}
         />
-        <h3>{this.state.login}</h3>
-        <a href={this.state.html_url} className='btn btn-dark btn-sm my-1'>
-          More
-        </a>
+        <h3 className='username'>{login}</h3>
+        <div>
+          <a href={html_url} className='btn btn-dark btn-sm my-1'>
+            More
+          </a>
+        </div>
       </div>
     );
   }
