fc248bc7b5d78fb06409bb87fc99fe6a17dea8be
ali-fadaei23 <razorn1375@gmail.com>
2024-05-08 12:41:07 +0330

    Refactoring components of app and serach

--- src/components/users/Search.jsx
+++ src/components/users/Search.jsx
@@ -9,6 +9,7 @@ export class Search extends Component {
   static propTypes = {
     searchUsers: PropTypes.func.isRequired,
     clearUsers: PropTypes.func.isRequired,
+    showClear: PropTypes.bool.isRequired,
   };
 
   onChange = (e) => this.setState({ [e.target.name]: e.target.value });
@@ -20,6 +21,7 @@ export class Search extends Component {
   };
 
   render() {
+    const { showClear, clearUsers } = this.props;
     return (
       <div>
         <form className='form' onSubmit={this.onSubmit}>
@@ -37,12 +39,11 @@ export class Search extends Component {
             className='btn btn-dark btn-block'
           />
         </form>
-        <button
-          className='btn btn-light btn-block'
-          onClick={this.props.clearUsers}
-        >
-          Clear
-        </button>
+        {showClear && (
+          <button className='btn btn-light btn-block' onClick={clearUsers}>
+            Clear
+          </button>
+        )}
       </div>
     );
   }
